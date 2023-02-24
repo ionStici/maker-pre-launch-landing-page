445b90287af3299ec655600afe5591f6881342d9
Ion Stici <ionsticidev@gmail.com>
2023-02-24 11:08:31 +0200

    Add form validation, run build

--- src/components/GetNotifiedComponent.js
+++ src/components/GetNotifiedComponent.js
@@ -3,11 +3,39 @@ import { GetNotified } from "../containers/GetNotified";
 import squiggle from "./../assets/bg-footer-squiggle.svg";
 
 export class GetNotifiedComponent extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+      document
+        .querySelector(".input")
+        .value.match(
+          /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        )
+    ) {
+      document.querySelector(".input").style.border =
+        "2px solid var(--color-white)";
+      document.querySelector(".errorMessage").style.display = "none";
+      document.querySelector(".errorMessage").style.visibility = "hidden";
+
+      setTimeout(() => {
+        document.querySelector(".input").value = "";
+        document.querySelector(".notification").textContent = "See Ya!";
+      }, 500);
+    } else {
+      document.querySelector(".input").style.border =
+        "2px solid var(--color-red)";
+      document.querySelector(".errorMessage").style.display = "revert";
+      document.querySelector(".errorMessage").style.visibility = "revert";
+    }
+  }
+
   render() {
     return (
       <GetNotified
         squiggle={squiggle}
         error="Oops! That doesn’t look like an email address"
+        onSubmit={this.handleSubmit}
       />
     );
   }
--- src/containers/GetNotified.js
+++ src/containers/GetNotified.js
@@ -5,11 +5,18 @@ export class GetNotified extends React.Component {
   render() {
     return (
       <footer className={styles.footer}>
-        <h2 className={styles.h2}>Get notified when we launch</h2>
-        <form className={styles.form}>
-          <input className={styles.input} placeholder="Email address" />
+        <h2 className={`${styles.h2} notification`}>
+          Get notified when we launch
+        </h2>
+        <form onSubmit={this.props.onSubmit} className={`${styles.form} form`}>
+          <input
+            className={`${styles.input} input`}
+            placeholder="Email address"
+          />
           <button className={styles.btn}>Get notified</button>
-          <p className={styles.errorMessage}>{this.props.error}</p>
+          <p className={`${styles.errorMessage} errorMessage`}>
+            {this.props.error}
+          </p>
         </form>
         <img className={styles.img} src={this.props.squiggle} alt="" />
       </footer>
